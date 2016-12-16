b40eba840e2bbcd337e39b02c40e399b1296a772
Benson Polican <bensonpolican@yahoo.com>
2016-12-16 12:43:47 +0800

    no message

---
+++ a.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+typedef char string10 [10];
+typedef char string50 [51];
+typedef char string200 [200];
+
+typedef struct
+{
+	string50 title;
+	string200 authors;
+	string50 publicationDate;
+	int callNumber;
+	string50 materialType;
+	int numberOfCopies;
+}book;
+
+typedef struct
+{
+	string50 name;
+	string;
+	string50 publicationDate;
+	int callNumber;
+	string50 materialType;
+	int numberOfCopies;
+}book;
+
+int main(){
+	
+	printf("fuck");
+	return 0;
+}
